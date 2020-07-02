cd8ea833d4a6a8b7415c542beb87a70aa3b79b9c
Kai Wittmann <kaisky89@gmail.com>
2020-07-02 21:26:47 +0200

    make port configurable through .env

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
+import * as env from 'env-var'
+
+const port = env.get('PORT').default('4000').asPortNumber()
 
 new ApolloServer({ schema, context: createContext, cors: true }).listen(
-  { port: 4000 },
+  { port },
   () =>
     console.log(
-      '🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api'
+      `🚀 Server ready at: http://localhost:${port}\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`
     )
 )
